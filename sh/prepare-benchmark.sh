2994f03554707524f12498f33a482bb5b2ddffd7
Wing Lung Ngai <w.l.ngai@vu.nl>
2017-06-13 20:14:18 +0200

    Enforce loading intel compiler modules.

--- sh/prepare-benchmark.sh
+++ sh/prepare-benchmark.sh
@@ -48,6 +48,12 @@ if [ -z $GRAPHMAT_HOME ]; then
     exit 1
 fi
 
+module unload openmpi
+module unload openmpi/gcc
+module load intel/compiler
+module load intel-mpi
+module load intel/mkl
+
 mkdir -p bin/standard
 (cd bin/standard && cmake -DCMAKE_BUILD_TYPE=Release ../../src/main/c -DGRAPHMAT_HOME=$GRAPHMAT_HOME && make all VERBOSE=1)
 
